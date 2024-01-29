259e472b15829a5a3829221f57e6e8a809c1d6a5
Srujith <srujith@kudikala.com>
2024-01-28 21:59:53 -0500

    retain settings data in localstorage

--- ui/src/Settings.js
+++ ui/src/Settings.js
@@ -2,16 +2,20 @@ import React from 'react'
 import { useEffect, useState } from 'react';
 import { Button, Form, Modal } from 'react-bootstrap';
 
-const updateSetting = (key, value) => {
-    localStorage.setItem(key, value)
+const updateSettings = (event) => {
+    event.preventDefault();
+    localStorage.setItem("navidrome_server_url", event.target.url.value)
+    localStorage.setItem("navidrome_username", event.target.username.value)
+    localStorage.setItem("navidrome_password", event.target.password.value)
+    localStorage.setItem("limit", event.target.limit.value)
+    localStorage.setItem("token", event.target.token.value)
     localStorage.removeItem("songs")
 }
 
-const Settings = () => {
+const Settings = (props) => {
     const [isLoading, setLoading] = useState(false);
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    let connectionState = null;
+    const [dialogMessage, setDialogMessage] = useState("");
+    const handleClose = () => setDialogMessage("")
     useEffect(() => {
         function testConnection() {
             return new Promise((resolve) => {
@@ -21,53 +25,63 @@ const Settings = () => {
                 let response = fetch("/api/test-login",
                 {
                     method: 'POST',
-                    body: {
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
                         "navidrome_server_url": navidrome_server_url ? navidrome_server_url : "https://example.com/navidrome",
                         "navidrome_username": navidrome_username ? navidrome_username : "user",
                         "navidrome_password": navidrome_password ? navidrome_password : "password",
+                    })
+                })
+                return response.then(response => {
+                    if (response.ok){
+                        return response.json()
+                    } else {
+                        setLoading(false);
+                        setDialogMessage("Got invalid response from Navidrome server")
                     }
                 })
-                return response.then(response => response.json())
             });
         }
 
         if (isLoading) {
             testConnection().then((test_response) => {
-                connectionState = test_response;
                 setLoading(false);
-                setShow(true);
+                setDialogMessage("Navidrome server connected sucessfully")
             });
         }
     }, [isLoading]);
     
     const handleClick = () => setLoading(true);
+    let showDialog = dialogMessage.length == 0?false:true
     return (
     <>
-    <Modal show={show} onHide={handleClose}>
+    <Modal show={showDialog} onHide={handleClose}>
         <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
+            <Modal.Title>Connection status</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Body>{dialogMessage}</Modal.Body>
         <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
             Close
             </Button>
         </Modal.Footer>
     </Modal>
-    <Form>
+    <Form onSubmit={(event)=>updateSettings(event)}>
         <Form.Group className='mb-3'>
             <Form.Label>Navidrome URL</Form.Label>
-            <Form.Control onChange={(event) => {updateSetting("navidrome_server_url",event.target.value)}} placeholder='https://example.com/navidrome'/>
+            <Form.Control name='url' defaultValue={localStorage.getItem("navidrome_server_url")} placeholder='https://example.com/navidrome'/>
             <Form.Text className='text-muted'>The base url of the Navidrome installation</Form.Text>
         </Form.Group>
         <Form.Group className='mb-3'>
             <Form.Label>Navidrome username</Form.Label>
-            <Form.Control onChange={(event) => {updateSetting("navidrome_username",event.target.value)}} placeholder='user'/>
-            <Form.Text className='text-muted'>Navidrome username</Form.Text>
+            <Form.Control defaultValue={localStorage.getItem("navidrome_username")} name='username' placeholder='user'/>
+            <Form.Text  className='text-muted'>Navidrome username</Form.Text>
         </Form.Group>
         <Form.Group className='mb-3'>
             <Form.Label>Navidrome password</Form.Label>
-            <Form.Control type="password" onChange={(event) => {updateSetting("navidrome_password",event.target.value)}} placeholder='password'/>
+            <Form.Control defaultValue={localStorage.getItem("navidrome_password")} name='password' type="password" placeholder='password'/>
             <Form.Text className='text-muted'>Navidrome password</Form.Text>
         </Form.Group>
 
@@ -77,13 +91,16 @@ const Settings = () => {
 
         <Form.Group className='mb-3'>
             <Form.Label>LastFM Token</Form.Label>
-            <Form.Control onChange={(event) => {updateSetting("lastfm_token",event.target.value)}} placeholder='token'/>
+            <Form.Control defaultValue={localStorage.getItem("token")} name='token' placeholder='token'/>
             <Form.Text className='text-muted'>LastFM token to use for querying similar songs</Form.Text>
         </Form.Group>
         <Form.Group className='mb-3'>
             <Form.Label>Limit</Form.Label>
-            <Form.Control onChange={(event) => {updateSetting("limit",event.target.value)}} placeholder='100'/>
+            <Form.Control defaultValue={localStorage.getItem("limit")} name='limit' placeholder='100'/>
             <Form.Text className='text-muted'>Number of songs to suggest</Form.Text>
+        </Form.Group>
+        <Form.Group className='mb-3'>
+            <Button type="submit">Save</Button>
         </Form.Group>
     </Form>
     </>)
